f959ef5b01e6779d432c6149eaf43e484160df71
Makesh170399 <makeshravindran99@gmail.com>
2022-11-15 21:35:01 +0530

    First Commit

---
+++ src/person/Person.java
@@ -0,0 +1,14 @@
+package person;
+
+public class Person {
+	
+	public static void main(String[] args) {
+
+	String str="Hungry";
+	if(str.equalsIgnoreCase("hungry")) {
+		System.out.println("Eat");
+	}
+	else 
+		System.out.println("Take Rest");
+}
+}
